ec4fa3dc23bf93edc7c48a91d87de9f7ef8343a6
Thouin <mixmax109@hotmail.com>
2020-01-31 22:27:05 -0500

    MenuPause+Options

---
+++ HomeWreaker/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameISPAused = false;
+    public GameObject pauseMenuUi;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameISPAused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameISPAused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameISPAused = true;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
