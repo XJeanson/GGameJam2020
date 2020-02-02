45cb7d57fb10a15bc60c1bbb92f0eeb6ec9a2fa5
Xavier Jeanson <xavier_jeanson@hotmail.ca>
2020-02-02 00:00:22 -0500

    update player movement

--- HomeWreaker/Assets/Scripts/Player2Movements.cs
+++ HomeWreaker/Assets/Scripts/Player2Movements.cs
@@ -12,6 +12,9 @@ public class Player2Movements : MonoBehaviour
     public float gravity = 20.0f;
 
     private Vector3 moveDirection = Vector3.zero;
+    private float horizontal;
+    private float vertical;
+    private Vector3 direction;
 
     void Start()
     {
@@ -20,6 +23,8 @@ public class Player2Movements : MonoBehaviour
 
     void Update()
     {
+        horizontal = Input.GetAxis("P2Horizontal");
+        vertical = Input.GetAxis("P2Vertical");
         if (characterController.isGrounded)
         {
             // We are grounded, so recalculate
@@ -28,6 +33,7 @@ public class Player2Movements : MonoBehaviour
             //Debug.Log(Input.GetAxis("P2Horizontal"));
             //Debug.Log(Input.GetAxis("P2Vertical"));
 
+            direction = new Vector3(horizontal, 0, vertical);
             moveDirection = new Vector3(Input.GetAxis("P2Horizontal"), 0.0f, Input.GetAxis("P2Vertical"));
             moveDirection *= speed;
 
@@ -44,5 +50,6 @@ public class Player2Movements : MonoBehaviour
 
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
+        characterController.transform.LookAt(transform.position + direction);
     }
 }
